24ea333d294311985cf8b572d510737928b68ab4
Ana Grisham <anaunique@icloud.com>
2023-07-18 16:26:18 -0700

    db js completed

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,8 +3,6 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('hit')
-    console.log("event" + event)
     event.preventDefault();
     // Store the triggered events
     window.deferredPrompt = event;
@@ -33,6 +31,5 @@ butInstall.addEventListener('click', async () => {
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
         // Clear prompt
-        console.log('install hit')
         window.deferredPrompt = null;
 });
